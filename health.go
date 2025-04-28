004d10866618e92edbdd7db9a56d0e4dbe6c8b4d
Louis Fradin <lerenn@users.noreply.github.com>
2025-04-28 13:26:57 +0200

    fix: fix server start and shutdown (#2)

--- health.go
+++ health.go
@@ -1,6 +1,8 @@
 package health
 
 import (
+	"context"
+	"errors"
 	"net/http"
 	"sync/atomic"
 )
@@ -8,11 +10,16 @@ import (
 // Health is a health checker.
 type Health struct {
 	isReady atomic.Value
+	addr    string
+	srv     *http.Server
 }
 
-// NewHealth returns a new health checker.
-func NewHealth() (*Health, error) {
+// New returns a new health checker.
+func New(addr string) (*Health, error) {
 	var h Health
+
+	// Set the address
+	h.addr = addr
 
 	// Readiness to false
 	h.isReady.Store(false)
@@ -25,10 +32,48 @@ func (h *Health) Ready(isReady bool) {
 	h.isReady.Store(isReady)
 }
 
-// HTTPServe starts the health checker.
-func (h *Health) HTTPServe() {
+// Serve starts the health checker synchronously.
+func (h *Health) Serve() error {
+	// Check if the server is started
+	if h.srv != nil {
+		return errors.New("server already started")
+	}
+
+	// Register the handlers
 	http.HandleFunc("/liveness", h.liveness())
 	http.HandleFunc("/readiness", h.readiness())
+
+	// Start the server
+	// NOTE: This is a blocking call
+	h.srv = &http.Server{Addr: h.addr}
+	err := h.srv.ListenAndServe()
+	if err != nil && !errors.Is(err, http.ErrServerClosed) {
+		return err
+	}
+
+	return nil
+}
+
+// Shutdown stops the health checker.
+func (h *Health) Shutdown(ctx context.Context) error {
+	// Check if the server is started
+	if h.srv == nil {
+		return errors.New("server not started")
+	}
+
+	// Set the readiness to false just in case
+	h.Ready(false)
+
+	// Shutdown the server
+	err := h.srv.Shutdown(ctx)
+	if err != nil {
+		return err
+	}
+
+	// Set the server to nil
+	h.srv = nil
+
+	return nil
 }
 
 func (h *Health) liveness() http.HandlerFunc {
